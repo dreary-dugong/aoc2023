15580d738f18d89dd58dc8cf196a459a2480de72
Daniel Gysi <danielgysi@protonmail.com>
2023-12-07 00:08:39 -0500

    solve day 6 part 1. Math is cool *sunglasses cat emoji*

--- 06/1/aoc6lvl1/src/lib.rs
+++ 06/1/aoc6lvl1/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use std::iter;
 use std::path::PathBuf;
 
 extern crate clap;
@@ -54,12 +55,48 @@ pub fn run(cfg: Config) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn parse(input: String) -> anyhow::Result<String> {
-    // remember to change the return type
-    todo!()
+/// parse the input data into races
+fn parse(input: String) -> anyhow::Result<Vec<Race>> {
+    // seperate lines
+    let mut lines_iter = input.lines();
+    let time_line = lines_iter
+        .next()
+        .ok_or(anyhow::anyhow!("Missing first line in file"))?;
+    let dist_line = lines_iter
+        .next()
+        .ok_or(anyhow::anyhow!("Missing second line in file"))?;
+
+    // associate values and make race structs
+    // man I still cannot figure out how to throw errors from closures
+    Ok(
+        iter::zip(time_line.split_whitespace(), dist_line.split_whitespace())
+            .skip(1)
+            .map(|(t, d)| Race {
+                time: t.parse::<f32>().expect("failed to parse number"),
+                distance: d.parse::<f32>().expect("failed to parse number"),
+            })
+            .collect(),
+    )
 }
 
-fn process(data: String) -> u32 {
-    // remember to change the param type
-    todo!()
+fn process(races: Vec<Race>) -> u32 {
+    races
+        .into_iter()
+        .map(|race| {
+            // we're using the quadratic formula to find the two roots, then counting the integers between
+            let sqrt_term = (race.time * race.time - 4_f32 * race.distance).sqrt();
+            let b_term = -1_f32 * race.time;
+            let a_term = -2_f32;
+
+            let lower_root = (b_term + sqrt_term) / a_term;
+            let upper_root = (b_term - sqrt_term) / a_term;
+
+            (upper_root.ceil() - (lower_root.floor() + 1_f32)) as u32
+        })
+        .product()
+}
+
+struct Race {
+    time: f32,
+    distance: f32,
 }
